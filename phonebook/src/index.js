61d77405f8e7f0add0d83cd2d4e9f0c6dc378dac
victorreverter <victorreverter@gmail.com>
2020-08-12 06:00:45 +0200

    First commit phonebook

---
+++ phonebook/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h2>This is a test and Work in progress...</h2>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
